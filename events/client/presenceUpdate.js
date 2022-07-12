a8524cdc5cd64040d78050420692228a437f2935
Bastian Leicht <mail@bastianleicht.de>
2022-07-12 12:48:46 +0200

    Updated Canvas Size, Text dimensions and Placement of Avatar.

--- events/client/presenceUpdate.js
+++ events/client/presenceUpdate.js
@@ -1,6 +1,7 @@
 const Canvas = require("canvas");
 const Discord = require("discord.js");
 const { registerFont, createCanvas } = require('canvas')
+registerFont('./whitney-bold.otf', { family: 'Whitney' })
 
 //here the event starts
 module.exports = async (client, oldMember, newMember) => {
@@ -8,46 +9,43 @@ module.exports = async (client, oldMember, newMember) => {
     //console.log(newMember);
     console.log(`Old Presence: ${oldMember.status} | New Presence: ${newMember.status}`);
 
-    const canvas = Canvas.createCanvas(1772, 633);
+    const canvas = Canvas.createCanvas(395, 80);
     //make it "2D"
     const ctx = canvas.getContext('2d');
     //set the Background to the welcome.png
-    const background = await Canvas.loadImage(`./welcome.png`);
+
+    // 395 x 80 px
+    const background = await Canvas.loadImage(`./banner1.png`);
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = '#f2f2f2';
+    //ctx.strokeStyle = '#f2f2f2';
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
-    //set the first text string
-    var textString3 = `${newMember.user.username}`;
+    //define the Username
+    const username = `${newMember.user.username}#${newMember.user.discriminator}`;
     //if the text is too big then smaller the text
-    if (textString3.length >= 14) {
-        ctx.font = 'bold 100px "Roboto"';
-        ctx.fillStyle = '#f2f2f2';
-        ctx.fillText(textString3, 720, canvas.height / 2 + 20);
+    if (username.length >= 14) {
+        ctx.font = 'bold 15px "Whitney"';
+        ctx.fillStyle = '#bec1c6';
+        ctx.fillText(username, 88, canvas.height / 2 - 20);
     }
     //else dont do it
     else {
-        ctx.font = 'bold 150px "Roboto"';
+        ctx.font = 'bold 30px "Whitney"';
         ctx.fillStyle = '#f2f2f2';
-        ctx.fillText(textString3, 720, canvas.height / 2 + 20);
+        ctx.fillText(username, 70, canvas.height / 2 + 20);
     }
-    //define the Discriminator Tag
-    var textString2 = `#${newMember.user.discriminator}`;
-    ctx.font = 'bold 40px "Roboto"';
-    ctx.fillStyle = '#f2f2f2';
-    ctx.fillText(textString2, 730, canvas.height / 2 + 58);
     //define the Member count
     var textString4 = `Member #${newMember.guild.memberCount}`;
     ctx.font = 'bold 60px "Roboto"';
     ctx.fillStyle = '#f2f2f2';
-    ctx.fillText(textString4, 750, canvas.height / 2 + 125);
+    ctx.fillText(textString4, 10, canvas.height / 2 + 125);
     //get the Guild Name
     var textString4 = `${newMember.guild.name}`;
     ctx.font = 'bold 60px "Roboto"';
     ctx.fillStyle = '#f2f2f2';
-    ctx.fillText(textString4, 700, canvas.height / 2 - 150);
+    ctx.fillText(textString4, 30, canvas.height / 2 - 150);
     //create a circular "mask"
     ctx.beginPath();
-    ctx.arc(315, canvas.height / 2, 250, 0, Math.PI * 2, true);//position of img
+    ctx.arc(40, canvas.height / 2, 34, 0, Math.PI * 2, true);//position of img
     ctx.closePath();
     ctx.clip();
 
@@ -55,10 +53,10 @@ module.exports = async (client, oldMember, newMember) => {
     const avatar = await Canvas.loadImage(newMember.user.displayAvatarURL({ format: 'jpg' }));
 
     //draw the avatar
-    ctx.drawImage(avatar, 65, canvas.height / 2 - 250, 500, 500);
+    ctx.drawImage(avatar, 5, (canvas.height / 2) - 35, 68, 68);
 
     //get it as a discord attachment
-    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'image.png');
 
     //define the welcome channel
     const channel = client.channels.cache.get('996038161153146882');
