bd23d0f63c81646028f81ccdba4bb0a87a1b75b4
Bastian Leicht <mail@bastianleicht.de>
2022-07-13 14:04:30 +0200

    Rendering Avatar and Status Dot over Circles

--- events/client/presenceUpdate.js
+++ events/client/presenceUpdate.js
@@ -135,8 +135,36 @@ module.exports = async (client, oldMember, newMember) => {
     //define the user avatar
     const avatar = await Canvas.loadImage(newMember.user.displayAvatarURL({ format: 'jpg' }));
 
-    //draw the avatar
-    ctx.drawImage(avatar, 5, (canvas.height / 2) - 35, 68, 68);
+    // Create Avatar Circle
+    ctx.save();
+    ctx.beginPath();
+    ctx.arc(40, (canvas.height / 2), 34, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.clip();
+
+    // Render Image in Circle
+    ctx.drawImage(avatar, 7, (canvas.height / 2) - 35, 68, 68);
+
+
+    ctx.beginPath();
+    ctx.arc(5, (canvas.height / 2) - 35, 34, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.clip();
+    ctx.restore();
+
+    // Draw Status Circle
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(62, (canvas.height / 2) + 21, 7, 0, 2 * Math.PI);
+    ctx.fill();
+    // Outline with background Color
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = '#1b1e21';
+    ctx.stroke();
+
+    //draw the status icon
+    //const s_icon = await Canvas.loadImage(status_icon);
+    //ctx.drawImage(s_icon, 55, (canvas.height / 2) + 15, 14, 14);
 
     //get it as a discord attachment
     const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'image.png');
